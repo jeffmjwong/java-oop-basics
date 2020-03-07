32d1ce2efaaf1e7148309a86cd58bb682f58bf00
Jeff Wong <v-jwong1@expedia.com>
2020-03-07 12:28:10 +1000

    stub out deposit and withdraw method in Account class

--- src/main/java/com/pluralsight/basicoop/Account.java
+++ src/main/java/com/pluralsight/basicoop/Account.java
@@ -1,4 +1,13 @@
 package com.pluralsight.basicoop;
 
+import java.math.BigDecimal;
+
 public class Account {
+    public void deposit(BigDecimal amount) {
+
+    }
+
+    public void withdraw(BigDecimal amount) {
+
+    }
 }
