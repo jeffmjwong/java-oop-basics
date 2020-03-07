9d1a6928cc8020a67af44a95003ba8fdf0ad12c6
Jeff Wong <v-jwong1@expedia.com>
2020-03-07 14:20:17 +1000

    add verification functionality to Account class

--- src/main/java/com/pluralsight/basicoop/Account.java
+++ src/main/java/com/pluralsight/basicoop/Account.java
@@ -3,11 +3,19 @@ package com.pluralsight.basicoop;
 import java.math.BigDecimal;
 
 public class Account {
+    private boolean isVerified;
+
+    public void holderVerified() {
+        isVerified = true;
+    }
+
     public void deposit(BigDecimal amount) {
 
     }
 
     public void withdraw(BigDecimal amount) {
-
+        if (!isVerified) {
+            return;
+        }
     }
 }
--- src/main/java/com/pluralsight/basicoop/Main.java
+++ src/main/java/com/pluralsight/basicoop/Main.java
@@ -2,12 +2,5 @@ package com.pluralsight.basicoop;
 
 public class Main {
     public static void main(String[] args) {
-        final double d1 = 0.03;
-        final double d2 = 0.04;
-        System.out.println(d2 - d1);
-
-        final float f1 = 0.03f;
-        final float f2 = 0.04f;
-        System.out.println(f2 - f1);
     }
 }
