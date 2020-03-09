e2f937e1ec02376f316491cd9f5f65ce4389f7b8
Jeff Wong <v-jwong1@expedia.com>
2020-03-09 18:11:38 +1000

    example run of AliasBugDemo with reserve method

--- src/main/java/com/pluralsight/basicoop/AliasBugDemo.java
+++ src/main/java/com/pluralsight/basicoop/AliasBugDemo.java
@@ -1,11 +1,18 @@
 package com.pluralsight.basicoop;
 
+import java.math.BigDecimal;
+
 public class AliasBugDemo {
+    private void reserve(Money cost) {
+        System.out.println("Reserving an item costing " + cost);
+    }
+
     private void buy(Money wallet, Money cost) {
 
     }
 
     public void run() {
-
+        final Money m1 = new Money(new BigDecimal("20"), new Currency("d"));
+        reserve(m1);
     }
 }
