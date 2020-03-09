591b6c5bea1e56dfc76decad807c92e0ab395351
Jeff Wong <v-jwong1@expedia.com>
2020-03-09 16:59:49 +1000

    finish up Currency class

--- src/main/java/com/pluralsight/basicoop/Currency.java
+++ src/main/java/com/pluralsight/basicoop/Currency.java
@@ -1,4 +1,25 @@
 package com.pluralsight.basicoop;
 
-public class Currency {
+import java.math.BigDecimal;
+
+public class Currency implements Comparable<Currency> {
+    private String symbol;
+
+    public Currency(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public Money zero() {
+        return new Money(BigDecimal.ZERO, this);
+    }
+
+    @Override
+    public String toString() {
+        return this.symbol;
+    }
+
+    @Override
+    public int compareTo(Currency otherCurrency) {
+        return this.symbol.compareTo(otherCurrency.symbol);
+    }
 }
