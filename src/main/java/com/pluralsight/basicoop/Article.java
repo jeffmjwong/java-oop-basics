b0267951c501b8a8d0e77324d230436c9c823bb4
Jeff Wong <v-jwong1@expedia.com>
2020-03-10 18:46:33 +1000

    complete implementation of Article class

--- src/main/java/com/pluralsight/basicoop/Article.java
+++ src/main/java/com/pluralsight/basicoop/Article.java
@@ -1,4 +1,18 @@
 package com.pluralsight.basicoop;
 
 public class Article {
+    private Warranty moneyBackGuarantee;
+    private Warranty expressWarranty;
+
+    public Article(Warranty moneyBackGuarantee, Warranty expressWarranty) {
+        this.moneyBackGuarantee = moneyBackGuarantee;
+        this.expressWarranty = expressWarranty;
+    }
+
+    public Warranty getMoneyBackGuarantee() {
+        return moneyBackGuarantee;
+    }
+    public Warranty getExpressWarranty() {
+        return expressWarranty;
+    }
 }
