320ddb5e9ad42eb99c1fa7dd22075bac59fa66cf
Jeff Wong <v-jwong1@expedia.com>
2020-03-10 20:15:05 +1000

    do both boolean flag and null checks

--- src/main/java/com/pluralsight/basicoop/WarrantyDemo.java
+++ src/main/java/com/pluralsight/basicoop/WarrantyDemo.java
@@ -3,8 +3,18 @@ package com.pluralsight.basicoop;
 import java.time.LocalDate;
 
 public class WarrantyDemo {
-    public void claimWarranty(Article article) {
+    public void claimWarranty(Article article, boolean isInGoodCondition, boolean isNonOperational) {
         LocalDate today = LocalDate.now();
+
+        if (isInGoodCondition && isNonOperational && article.getMoneyBackGuarantee() != null && article.getMoneyBackGuarantee().isValidOn(today)) {
+            System.out.println("Offer money back!");
+        }
+
+        if (isNonOperational && article.getExpressWarranty() != null && article.getExpressWarranty().isValidOn(today)) {
+            System.out.println("Offer repair!");
+        }
+
+        System.out.println("------------------------");
     }
 
     public void run() {
