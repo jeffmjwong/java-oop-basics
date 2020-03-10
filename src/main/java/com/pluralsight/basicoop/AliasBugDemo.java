3c2c79c043a9f59f5b9012ab9545cebb3f002a6d
Jeff Wong <v-jwong1@expedia.com>
2020-03-10 17:21:09 +1000

    base money type is not an instance of derived class such as euro

--- src/main/java/com/pluralsight/basicoop/AliasBugDemo.java
+++ src/main/java/com/pluralsight/basicoop/AliasBugDemo.java
@@ -26,6 +26,18 @@ public class AliasBugDemo {
     }
 
     public void run() {
+//        useAliasBug();
+
+        final Currency eur = new Currency("EUR");
+        final Money euro20 = new Money(new BigDecimal("20"), eur);
+        final Euro euro20de = new Euro(new BigDecimal("20"), eur, "de");
+        System.out.println(euro20 instanceof Money);
+        System.out.println(euro20de instanceof Money);
+        System.out.println(euro20 instanceof Euro);
+        System.out.println(euro20de instanceof Euro);
+    }
+
+    private void useAliasBug() {
         final Currency usd = new Currency("USD");
         final Money usd30 = new Money(new BigDecimal("30"), usd);
         final Money usd20 = new Money(new BigDecimal("20"), usd);
@@ -43,12 +55,5 @@ public class AliasBugDemo {
 
         isHappyHour = true;
         buy(usd30, usd10);
-        System.out.println();
-
-        System.out.println(usd30.equals(usd20));
-        final Currency usdagain = new Currency("USD");
-        final Money usd30again = new Money(new BigDecimal("30"), usdagain);
-        System.out.println(usd30.equals(usd30again));
-        System.out.println();
     }
 }
