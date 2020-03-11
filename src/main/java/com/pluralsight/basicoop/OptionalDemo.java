429f1f7811f0587017d9cbdfab5ff82fe277dac1
Jeff Wong <v-jwong1@expedia.com>
2020-03-11 17:32:18 +1000

    create concrete Some class that extends MaybeString class

--- src/main/java/com/pluralsight/basicoop/OptionalDemo.java
+++ src/main/java/com/pluralsight/basicoop/OptionalDemo.java
@@ -5,7 +5,26 @@ import java.util.Optional;
 public class OptionalDemo {
     private static abstract class MaybeString {
         public abstract MaybeString toUpperCase();
-        public abstract String orElse();
+        public abstract String orElse(String substitute);
+    }
+
+    private static class Some extends MaybeString {
+        private String value;
+
+        public Some(String value) {
+            this.value = value;
+        }
+
+        @Override
+        public MaybeString toUpperCase() {
+            value = value.toUpperCase();
+            return this;
+        }
+
+        @Override
+        public String orElse(String substitute) {
+            return value;
+        }
     }
 
     private void display(MaybeString value) {
@@ -16,7 +35,7 @@ public class OptionalDemo {
 
     public void run() {
         display(null);
-        display("something");
+        display(new Some("something"));
         display("Making Your Java Code More Object-Oriented");
     }
 }
