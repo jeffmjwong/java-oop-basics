088e645841907ffa0ef9d9c218fabeebca270b82
Jeff Wong <v-jwong1@expedia.com>
2020-03-11 15:22:12 +1000

    introduce sensor and extendedWarranty in Article class

--- src/main/java/com/pluralsight/basicoop/Article.java
+++ src/main/java/com/pluralsight/basicoop/Article.java
@@ -1,15 +1,19 @@
 package com.pluralsight.basicoop;
 
+import java.time.LocalDate;
+
 public class Article {
     private Warranty moneyBackGuarantee;
     private Warranty expressWarranty;
     private Warranty effectiveExpressWarranty;
+    private Part sensor;
+    private Warranty extendedWarranty;
 
     public Article(Warranty moneyBackGuarantee, Warranty expressWarranty) {
-        this(moneyBackGuarantee, expressWarranty, Warranty.VOID);
+        this(moneyBackGuarantee, expressWarranty, Warranty.VOID, null, Warranty.VOID);
     }
 
-    private Article(Warranty moneyBackGuarantee, Warranty expressWarranty, Warranty effectiveExpressWarranty) {
+    private Article(Warranty moneyBackGuarantee, Warranty expressWarranty, Warranty effectiveExpressWarranty, Part sensor, Warranty extendedWarranty) {
         if (moneyBackGuarantee == null || expressWarranty == null) {
             throw new IllegalArgumentException("Constructor arguments cannot be null!");
         }
@@ -17,6 +21,8 @@ public class Article {
         this.moneyBackGuarantee = moneyBackGuarantee;
         this.expressWarranty = expressWarranty;
         this.effectiveExpressWarranty = effectiveExpressWarranty;
+        this.sensor = sensor;
+        this.extendedWarranty = extendedWarranty;
     }
 
     public Warranty getMoneyBackGuarantee() {
@@ -25,12 +31,23 @@ public class Article {
     public Warranty getExpressWarranty() {
         return effectiveExpressWarranty;
     }
+    public Warranty getExtendedWarranty() {
+        return extendedWarranty;
+    }
 
     public Article withVisibleDamage() {
-        return new Article(Warranty.VOID, expressWarranty, effectiveExpressWarranty);
+        return new Article(Warranty.VOID, expressWarranty, effectiveExpressWarranty, sensor, extendedWarranty);
     }
 
     public Article nonOperational() {
-        return new Article(moneyBackGuarantee, expressWarranty, expressWarranty);
+        return new Article(moneyBackGuarantee, expressWarranty, expressWarranty, sensor, extendedWarranty);
+    }
+
+    public Article install(Part sensor, Warranty extendedWarranty) {
+        return new Article(moneyBackGuarantee, expressWarranty, effectiveExpressWarranty, sensor, extendedWarranty);
+    }
+
+    public Article sensorNotOperational(LocalDate detectedOn) {
+        return install(sensor.defective(detectedOn), extendedWarranty);
     }
 }
