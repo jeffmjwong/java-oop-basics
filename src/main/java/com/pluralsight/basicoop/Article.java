a4cc400389da360ed72e7acfa6e0f58c792f5cef
Jeff Wong <v-jwong1@expedia.com>
2020-03-11 11:46:00 +1000

    create private Article constructor that takes 3 arguments

--- src/main/java/com/pluralsight/basicoop/Article.java
+++ src/main/java/com/pluralsight/basicoop/Article.java
@@ -6,12 +6,17 @@ public class Article {
     private Warranty effectiveExpressWarranty;
 
     public Article(Warranty moneyBackGuarantee, Warranty expressWarranty) {
+        this(moneyBackGuarantee, expressWarranty, Warranty.VOID);
+    }
+
+    private Article(Warranty moneyBackGuarantee, Warranty expressWarranty, Warranty effectiveExpressWarranty) {
         if (moneyBackGuarantee == null || expressWarranty == null) {
             throw new IllegalArgumentException("Constructor arguments cannot be null!");
         }
 
         this.moneyBackGuarantee = moneyBackGuarantee;
         this.expressWarranty = expressWarranty;
+        this.effectiveExpressWarranty = effectiveExpressWarranty;
     }
 
     public Warranty getMoneyBackGuarantee() {
