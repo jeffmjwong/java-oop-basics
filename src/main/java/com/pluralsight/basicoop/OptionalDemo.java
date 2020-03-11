5d8a8778f18ac0bc08f8901fc03de964df890365
Jeff Wong <v-jwong1@expedia.com>
2020-03-11 16:42:59 +1000

    complete implementation of OptionalDemo class using null checks

--- src/main/java/com/pluralsight/basicoop/OptionalDemo.java
+++ src/main/java/com/pluralsight/basicoop/OptionalDemo.java
@@ -1,4 +1,14 @@
 package com.pluralsight.basicoop;
 
 public class OptionalDemo {
+    private void display(String value) {
+        String printout = value == null ? "Nothing to show..." : value.toUpperCase();
+        System.out.println(printout);
+    }
+
+    public void run() {
+        display(null);
+        display("something");
+        display("Making Your Java Code More Object-Oriented");
+    }
 }
