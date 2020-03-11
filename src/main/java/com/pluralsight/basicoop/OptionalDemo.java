754c5ac35cedf2820b72a745ea44d7b4af02c3de
Jeff Wong <v-jwong1@expedia.com>
2020-03-11 18:29:20 +1000

    make MaybeString class generic

--- src/main/java/com/pluralsight/basicoop/OptionalDemo.java
+++ src/main/java/com/pluralsight/basicoop/OptionalDemo.java
@@ -4,27 +4,27 @@ import java.util.Optional;
 import java.util.function.Function;
 
 public class OptionalDemo {
-    private static abstract class MaybeString {
-        public abstract MaybeString map(Function<String, String> transform);
-        public abstract String orElse(String substitute);
+    private static abstract class Maybe<T> {
+        public abstract Maybe<T> map(Function<T, T> transform);
+        public abstract T orElse(T substitute);
         public abstract boolean isPresent();
-        public abstract String get();
+        public abstract T get();
     }
 
-    private static class Some extends MaybeString {
-        private String content;
+    private static class Some<T> extends Maybe<T> {
+        private T content;
 
-        public Some(String content) {
+        public Some(T content) {
             this.content = content;
         }
 
         @Override
-        public MaybeString map(Function<String, String> transform) {
-            return new Some(transform.apply(content));
+        public Maybe<T> map(Function<T, T> transform) {
+            return new Some<>(transform.apply(content));
         }
 
         @Override
-        public String orElse(String substitute) {
+        public T orElse(T substitute) {
             return content;
         }
 
@@ -34,21 +34,21 @@ public class OptionalDemo {
         }
 
         @Override
-        public String get() {
+        public T get() {
             return content;
         }
     }
 
-    private static class None extends MaybeString {
+    private static class None<T> extends Maybe<T> {
         public None() {}
 
         @Override
-        public MaybeString map(Function<String, String> transform) {
+        public Maybe<T> map(Function<T, T> transform) {
             return this;
         }
 
         @Override
-        public String orElse(String substitute) {
+        public T orElse(T substitute) {
             return substitute;
         }
 
@@ -58,18 +58,23 @@ public class OptionalDemo {
         }
 
         @Override
-        public String get() {
+        public T get() {
             throw new IllegalStateException();
         }
     }
 
-    private void display(MaybeString value) {
+    private void display(Maybe<String> value) {
         System.out.println(value.map(String::toUpperCase).orElse("Nothing to show..."));
     }
 
+    private void displayInt(Maybe<Integer> value) {
+        System.out.println(value.map(Integer::intValue).orElse(100));
+    }
+
     public void run() {
-        display(new None());
-        display(new Some("something"));
-        display(new Some("Making Your Java Code More Object-Oriented"));
+        display(new None<>());
+        display(new Some<>("something"));
+        display(new Some<>("Making Your Java Code More Object-Oriented"));
+        displayInt(new Some<>(20));
     }
 }
